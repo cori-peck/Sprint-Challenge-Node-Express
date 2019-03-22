0b566bf1b58dc30005e17636d3a4ab89bcf98096
cori-peck <cori.peck@ymail.com>
2019-03-22 15:44:22 -0500

    C-create(post) is functional for post actions and projects

--- data/routers/actions-router.js
+++ data/routers/actions-router.js
@@ -3,8 +3,34 @@ const express = require('express');
 const router = express.Router();
 
 const Actions = require('../helpers/actionModel');
+const Projects = require('../helpers/projectModel');
 
 
+//C - Create
+router.post('/', async (req, res) => {
+    try {
+        const { project_id, description, notes } = req.body;
+        if (!project_id || !description || !notes ) {
+            res.status(400).json({ message: "Action must have a project id, a description AND notes" });
+        } else {
+             await Projects.get(project_id)
+             .then (project => {
+                 if (project) {
+                     Actions.insert(req.body)
+                     .then(action => {
+                         return res.status(201).json(action);
+                     })
+                 }
+            }) .catch(err => {
+                res.status(404).json({ message: "That project id can not be found" })
+            })
+        }
+    } catch {
+        res.status(500).json({ error: "Could not add the action" });
+    }
+})
+
+//R - Read
 router.get('/', async (req, res) => {
     try {
         const actions = await Actions.get();
--- data/routers/projects-router.js
+++ data/routers/projects-router.js
@@ -5,6 +5,23 @@ const router = express.Router();
 const Projects = require('../helpers/projectModel');
 
 
+//C - Create
+router.post('/', async (req, res) => {
+    try {
+        const { name, description } = req.body
+        if (!name || !description) {
+            res.status(400).json({message: "A project name and description are both required" });
+        } else {
+            const newProject = await Projects.insert(req.body)
+            res.status(201).json(newProject);
+        }
+    } catch {
+        res.status(500).json({ error: "Cannot add Project" });
+    }
+})
+
+
+// R - Read
 router.get('/', async (req, res) => {
     try {
         const projects = await Projects.get();
